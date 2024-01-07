6089f73ea68c02bd7690d0733c86333b7ae6a43f
integral05 <tejastathe4337@gmail.com>
2024-01-07 16:35:18 +0530

    Added assignment5.py file

---
+++ assignment5.py
@@ -0,0 +1,181 @@
+#1.
+'''
+
+rows=int(input("Enter no. of rows:"))
+for i in range(rows):
+    for j in range(rows):
+        print("*",end=" ")
+    print()
+print()
+'''
+
+
+#2.
+
+'''
+rows=int(input("Enter no. of rows:"))
+num=1
+for i in range(rows):
+    for j in range(rows):
+        if(num<10):
+            print(num,end="  ")
+            num+=1
+        else:
+            print(num,end=" ")
+            num+=1
+    print()
+print()
+'''
+
+
+#3.
+
+'''
+rows=int(input("Enter no. of rows:"))
+for i in range(rows):
+    num=i+1
+    for j in range(rows):
+        print(num,end=" ")
+    print()
+print()
+'''
+
+
+#4.
+
+'''
+rows=int(input("Enter no. of rows:"))
+for i in range(rows):
+    num=1
+    for j in range(rows):
+        print(num,end=" ")
+        num+=1
+    print()
+print()
+'''
+
+
+#5.
+
+'''
+rows=int(input("Enter no. of rows:"))
+for  i in range(rows):
+    num=i+1
+    for j in range (rows):
+        print(num,end=" ")
+        num+=1
+    print()
+print()
+'''
+
+#6.
+
+'''
+rows=int(input("Enter no. of rows:"))
+for i in range(rows):
+    for j in range(rows):
+        if(i%2==0):
+            print("#",end="  ")
+        else:
+            print("@",end="  ")
+    print()
+print()
+'''
+
+#7.
+
+'''
+rows=int(input("Enter no. of rows:"))
+num=1
+for i in range(rows):
+    for  j in range(rows):
+        if(num<10):
+            print(num,end="  ")
+        else:
+            print(num,end=" ")
+        num+=2
+    print()
+print()
+'''
+
+
+#8.
+
+'''
+rows=int(input("Enter no. of rows:"))
+
+for i in range(rows):
+    num=1
+    for j in range(rows):
+        print(num,end=" ")
+        num+=2
+    print()
+print()
+'''
+
+
+#9.
+
+
+rows=int(input("Enter no. of rows:"))
+num=1
+for i in range(rows):
+    for j in range(rows):
+        print(num,end=" ")
+        num+=2
+    print()
+    num-=4
+print()
+
+
+
+#10.
+
+'''
+rows=int(input("Enter no. of rows:"))
+num=1
+for i in range(rows):
+    for j in range(rows):
+        if(num<10):
+            print(num,end="  ")
+        else:
+            print(num,end=" ")
+        num+=2
+    print()
+    num-=4
+print()
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
